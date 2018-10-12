9c2e63c6b851d061795e3bdd149254867b0caa54
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-12 22:28:40 +1100

    Added submitAction outline in the form of comments

--- src/submit-action.ts
+++ src/submit-action.ts
@@ -10,5 +10,13 @@ import { Request, Response } from "express";
  * @param response
  */
 export default function submitAction(request: Request, response: Response) : void {
+    // Validate everything, or render an error page with a "Try Again" link if the passed in data is invalid
+
+    // Sanitize all data for security
+
+    // Parse the request and create a HCard
+
+    // Pass off to saveHcard, which will handle all of the database logic itself. This is a request handler and should not handle database operations directly, or even know what a database is.
+
     response.redirect('/');
 }
