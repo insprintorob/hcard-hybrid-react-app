0d2f3bf4127e1f86d13c338ff8950d5fe30b15a4
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-12 22:19:54 +1100

    Abstracted away /submit action handler into a module

--- serve.ts
+++ serve.ts
@@ -1,15 +1,23 @@
 import * as express from 'express';
 import html from './src/html';
 import { Request, Response } from 'express';
+import saveHCard from './src/save-hcard';
+import submitAction from './src/submit-action';
 
-
+/**
+ * Initialise express
+ */
 let app = express();
 
-
+/**
+ * Serve dist assets statically
+ */
 app.use(express.static('dist'));
+
+/**
+ *
+ */
 app.get('/', (request: Request, response: Response) => response.send(html));
-app.post('/submit', (request: Request, response: Response) => {
-    response.redirect('/');
-});
+app.post('/submit', submitAction);
 
 app.listen(8080);
---
+++ src/submit-action.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+/**
+ * Handle the submit action. This is a "Controller Action" in the MVC design pattern.
+ *
+ * This module can be plugged into the "/submit" express route directly.
+ * All the logic to handle the submit route is abstraced away into this function.
+ *
+ * @param request
+ * @param response
+ */
+export default function submitAction(request: Request, response: Response) {
+    response.redirect('/');
+}
