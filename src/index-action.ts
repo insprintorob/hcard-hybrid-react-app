89d87690707781ab2dc21b8cbb35560d71524ee7
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-10-14 12:18:03 +1100

    Allow Request and Response to be mocked

--- src/index-action.ts
+++ src/index-action.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from 'express';
 import getHTML from './react-ssr';
 
-export default function indexAction(request : any, response: any) {
+/**
+ * Render the HTML output of the SSR React app from getHTML()
+ *
+ *
+ * Request and Response can be mocked for testing
+ * @param request Request | any
+ * @param response Response | any
+ */
+export default function indexAction(request : Request | any, response: Response | any) {
     getHTML().then((html : string) => {
         response.send(html);
     });
